bd80fc0272fde53429dd3a75c8b7d78b4eabc4eb
Clint Animbom <clint360.rebase@gmail.com>
2024-04-28 19:42:05 +0100

    automatic dashboard redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 "use client"
 import Navbar from "@/core/components/organisms/Navbar";
 import Sidebar from "@/core/components/organisms/Sidebar";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const router = useRouter()
+  
+  useEffect(() => {
+     router.push('/dashboard')
+  }, []);
 
   return (
     <main className={""}>
