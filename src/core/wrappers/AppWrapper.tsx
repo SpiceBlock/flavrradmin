63cc6d162e5d7fae7d5bdd1abbeb6d40d611b3ac
Clint Animbom <clint360.rebase@gmail.com>
2024-06-13 16:20:09 +0100

    finished bot

--- src/core/wrappers/AppWrapper.tsx
+++ src/core/wrappers/AppWrapper.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import styles from './AppWrapper.module.scss'
 import Navbar from '../components/organisms/Navbar';
 import Sidebar from '../components/organisms/Sidebar';
+import { redirect } from 'next/navigation';
+import { useAuth } from '../hooks/AuthContext';
+import { auth } from '../firebase/firebase';
 
 
 
 const AppWrapper = (Page: any, title: string): any => {
+  const currentUser = auth.onAuthStateChanged((user) => {return user})
+    console.log(currentUser)
+    if (!currentUser) {
+      redirect('/login');
+    } 
   async function AppPage() {
-
     return (
       <div className={styles.mainApp}  >
         <Navbar />
